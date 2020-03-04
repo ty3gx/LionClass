76c9f24102ce8d8667f578d5e9bca7adf8d9b804
Tianchang_Yang <ty2419@columbia.edu>
2020-03-04 13:11:32 -0500

    change root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :courses
+  root 'courses#index'
 
   get 'search', to: 'courses#search'
   # The priority is based upon order of creation: first created -> highest priority.
