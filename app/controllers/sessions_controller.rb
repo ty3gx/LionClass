54e73522fbab34e173664443a0addcf5e4295453
baike960 <kevinzzc@126.com>
2020-03-31 00:18:24 -0400

    fixed login message bug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,14 @@ class SessionsController < ApplicationController
 	protect_from_forgery with: :null_session
 	def create
 		user = User.find_by(email:login_params[:email])
-		if user && user.authenticate(login_params[:password])
+		if !user
+			flash[:login_errors] = ['User does not exist.']
+			redirect_to '/'
+		elsif user && user.authenticate(login_params[:password])
 			session[:user_id] = user.id
 			redirect_to '/calendar'
 		else
-			flash[:login_errors] = ['incorrect credentials']
+			flash[:login_errors] = ['Incorrect credentials']
 			redirect_to '/'
 		end
 	end
